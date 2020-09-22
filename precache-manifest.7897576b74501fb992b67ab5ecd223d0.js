e87be2f90f646bbb5e85cf32ac8239a0e29083c8
anurag704 <anuragjha1093@gmail.com>
2020-09-22 13:26:14 +0530

    Updates

---
+++ precache-manifest.7897576b74501fb992b67ab5ecd223d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d20711e2003a59bcd4d9440df7cf9cc",
+    "url": "/money-tracker/index.html"
+  },
+  {
+    "revision": "a705b64ca71eeca63cb2",
+    "url": "/money-tracker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "eb5e51dce0ba5e2c4e56",
+    "url": "/money-tracker/static/css/main.3dc78e12.chunk.css"
+  },
+  {
+    "revision": "a705b64ca71eeca63cb2",
+    "url": "/money-tracker/static/js/2.c7033874.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-tracker/static/js/2.c7033874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5e51dce0ba5e2c4e56",
+    "url": "/money-tracker/static/js/main.3062287e.chunk.js"
+  },
+  {
+    "revision": "acd2850899a0b3061707",
+    "url": "/money-tracker/static/js/runtime-main.37284696.js"
+  }
+]);
